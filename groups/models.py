72d819d10da89f61aa231d7554b9791a56e43930
Jason Lin <jasonx.1011@gmail.com>
2017-12-20 18:49:19 -0800

    initial templates and models of groups & posts app

---
+++ groups/models.py
@@ -0,0 +1,44 @@
+from django.core.urlresolvers import reverse
+from django.db import models
+from django.utils.text import slugify
+import misaka
+from django.contrib.auth import get_user_model
+from django import template
+
+
+# Create your models here.
+User = get_user_model()
+register = template.Library()
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(allow_unicode=True, unique=True)
+    description = models.TextField(blank=True, default='')
+    description_html = models.TextField(editable=False, blank=True, default='')
+    members = models.ManyToManyField(User, through='GroupMember')
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        self.description_html = misaka.html(self.description)
+        super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse('group:single', kwargs={'slug':self.slug})
+
+    class Meta:
+        ordering = ['name']
+
+
+class GroupMember(models.Model):
+    group = models.ForeignKey(Group, related_name='memberships')
+    user = models.ForeignKey(User, related_name='user_group')
+
+    def __str__(self):
+        return self.user.username
+
+    class Meta:
+        unique_together = ('group', 'user')
