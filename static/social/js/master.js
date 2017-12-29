b9461ec0d8d683f6bddd6dc443cc4f76bf105078
Jason Lin <jasonx.1011@gmail.com>
2017-12-28 18:15:46 -0800

    update css/js/base html

--- static/social/js/master.js
+++ static/social/js/master.js
@@ -1,6 +1,13 @@
-$(function() {
-		$('.pop').on('click', function() {
-			$('.imagepreview').attr('src', $(this).find('img').attr('src'));
-			$('#imagemodal').modal('show');
-		});
+$(function () {
+    $('.pop').on('click', function () {
+        $('.imagepreview').attr('src', $(this).find('img').attr('src'));
+        $('#imagemodal').modal('show');
+    });
+});
+
+$(function () {
+    $('.pop-text').on('click', function () {
+        $('.imagepreview').attr('src', $(this).find('h4').attr('src'));
+        $('#imagemodal').modal('show');
+    });
 });
