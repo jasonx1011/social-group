66f0cc81d40160102de3eb64b1924b81f89bb38d
Jason Lin <jasonx.1011@gmail.com>
2017-12-29 11:22:57 -0800

    adding many-to-many field

--- accounts/models.py
+++ accounts/models.py
@@ -1,14 +1,20 @@
 from django.db import models
 from django.contrib import auth
 from django.core.urlresolvers import reverse
+from django.contrib.auth import get_user_model
 from django.utils.text import slugify
 import misaka
 
 
 # Create your models here.
+
+
 class User(auth.models.User, auth.models.PermissionsMixin):
     image_url = models.URLField(default="http://via.placeholder.com/140x100")
     description = models.TextField(max_length=80, blank=True, default='')
+    relationships = models.ManyToManyField('self', through='Relationship',
+                                           symmetrical=False,
+                                           related_name='related_to')
 
     def __str__(self):
         return "@{}".format(self.username)
@@ -16,5 +22,52 @@ class User(auth.models.User, auth.models.PermissionsMixin):
     def get_absolute_url(self):
         return reverse('accounts:profile', kwargs={'pk': self.id})
 
+    def add_relationship(self, person, status):
+        relationship, created = Relationship.objects.get_or_create(
+            from_person=self,
+            to_person=person,
+            status=status)
+        return relationship
+
+    def remove_relationship(self, person, status):
+        Relationship.objects.filter(
+            from_person=self,
+            to_person=person,
+            status=status).delete()
+        return
+
+    def get_relationships(self, status):
+        return self.relationships.filter(
+            to_people__status=status,
+            to_people__from_person=self)
+
+    def get_related_to(self, status):
+        return self.related_to.filter(
+            from_people__status=status,
+            from_people__to_person=self)
+
+    def get_following(self):
+        return self.get_relationships(RELATIONSHIP_FOLLOWING)
+
+    def get_followers(self):
+        return self.get_related_to(RELATIONSHIP_FOLLOWING)
+
     class Meta:
         ordering = ['username']
+
+
+RELATIONSHIP_FOLLOWING = 1
+RELATIONSHIP_BLOCKED = 2
+RELATIONSHIP_STATUSES = (
+    (RELATIONSHIP_FOLLOWING, 'Following'),
+    (RELATIONSHIP_BLOCKED, 'Blocked'),
+)
+
+
+class Relationship(models.Model):
+    from_person = models.ForeignKey(User, related_name='from_people')
+    to_person = models.ForeignKey(User, related_name='to_people')
+    status = models.IntegerField(choices=RELATIONSHIP_STATUSES)
+
+
+
