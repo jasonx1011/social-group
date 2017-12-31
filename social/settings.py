4ce3ec534dc17b1f366f96b695b99fd06c6c5f33
Jason Lin <jasonx.1011@gmail.com>
2017-12-30 23:18:19 -0800

    adding Django-Heroku package

--- social/settings.py
+++ social/settings.py
@@ -160,3 +160,7 @@ STATICFILES_DIRS = (
 # https://warehouse.python.org/project/whitenoise/
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
