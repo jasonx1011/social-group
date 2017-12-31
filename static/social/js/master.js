5e38bdd9bc828870e809230149c6f6ab9b6104b0
Jason Lin <jasonx.1011@gmail.com>
2017-12-30 18:50:26 -0800

    add profile page

--- static/social/js/master.js
+++ static/social/js/master.js
@@ -11,3 +11,11 @@ $(function () {
         $('#imagemodal').modal('show');
     });
 });
+
+$(document).ready(function() {
+$(".btn-pref .btn").click(function () {
+    $(".btn-pref .btn").removeClass("btn-primary").addClass("btn-default");
+    // $(".tab").addClass("active"); // instead of this do the below
+    $(this).removeClass("btn-default").addClass("btn-primary");
+});
+});
