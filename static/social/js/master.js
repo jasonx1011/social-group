c7630f17513eae950ddda632ca2edf37d1c32c7c
Jason Lin <jasonx.1011@gmail.com>
2018-01-02 17:27:40 -0800

    add guest loging

--- static/social/js/master.js
+++ static/social/js/master.js
@@ -5,17 +5,17 @@ $(function () {
     });
 });
 
-$(function () {
-    $('.pop-text').on('click', function () {
-        $('.imagepreview').attr('src', $(this).find('h4').attr('src'));
-        $('#imagemodal').modal('show');
+$(document).ready(function () {
+    $(".btn-pref .btn").click(function () {
+        $(".btn-pref .btn").removeClass("btn-primary").addClass("btn-default");
+        // $(".tab").addClass("active"); // instead of this do the below
+        $(this).removeClass("btn-default").addClass("btn-primary");
     });
 });
 
-$(document).ready(function() {
-$(".btn-pref .btn").click(function () {
-    $(".btn-pref .btn").removeClass("btn-primary").addClass("btn-default");
-    // $(".tab").addClass("active"); // instead of this do the below
-    $(this).removeClass("btn-default").addClass("btn-primary");
-});
-});
+function fillValues() {
+    document.getElementById("id_username").value = "Guest";
+    document.getElementById("id_password").value = "guestguest123123";
+}
+
+$("#guest").click(fillValues);
